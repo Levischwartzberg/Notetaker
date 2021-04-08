2a4f63e4434f9937f32f038461c1422132e0bddd
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-04-08 18:48:00 -0500

    added initial files with a fair bit of functionality

---
+++ server.js
@@ -0,0 +1,43 @@
+const generateUniqueId = require('generate-unique-id');
+
+const express = require('express');
+const { get } = require('https');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3500;
+
+notes = [];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); 
+
+app.use(express.static('public'));
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.get('/api/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+    // console.log(req.body)
+    let note = req.body;
+    notes.push(note);
+    console.log(notes);
+    res.json(note);
+    let writeStream = fs.createWriteStream('db/db.txt');
+    writeStream.write(JSON.stringify(notes), 'utf8');
+    writeStream.on('finish', () => {
+      console.log('wrote all data to file');
+    });
+}) 
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
